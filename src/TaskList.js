8889ee9d688d0660487ea29016651ab35fbda36d
Adym <saalutlume@gmail.com>
2025-02-07 16:04:39 +0200

    I just wrote at the top starting to project

--- src/TaskList.js
+++ src/TaskList.js
@@ -1,3 +1,4 @@
+//starting to project 
 import React, { useState, useEffect } from "react";
 import styles from "./TaskList.module.css";
 
--- src/message.js
+++
@@ -1,4 +0,0 @@
-function Message(){
-    return <h1>This is new component!</h1>;
-}
-export default Message;
